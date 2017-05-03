657563f20e90d975da260a12a80fcfbb287b4ec2
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-05-03 14:56:38 -0700

    bumped version, requirements and added missing component name.

--- version.php
+++ version.php
@@ -1,3 +1,11 @@
 <?php
-$plugin->version = 2014070700;  // YYYYMMDDHH (year, month, day, 24-hr time)
-$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+/**
+ * Version details
+ *
+ * @package    block_course_favourites
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2016120500;
+$plugin->requires  = 2016112900;
+$plugin->component = 'block_course_favourites';
